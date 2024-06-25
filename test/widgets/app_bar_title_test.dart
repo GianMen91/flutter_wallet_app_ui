ecf25aabf5d04710840245636dc70a738cfe7610
GianMen91 <giancarlo.mennillo@gmail.com>
2024-06-25 10:52:24 +0200

    fix test

--- test/widgets/app_bar_title_test.dart
+++ test/widgets/app_bar_title_test.dart
@@ -19,14 +19,6 @@ void main() {
     expect(find.byKey(const Key('appBarTitle')), findsOneWidget);
 
     // Verify the text spans are correct.
-    expect(find.text('My '), findsOneWidget);
-    expect(find.text('Cards'), findsOneWidget);
-
-    // Verify the text styles are correct.
-    final textSpanFinder = find.byKey(const Key('appBarTitle')).evaluate().single.widget as RichText;
-    final textSpan = textSpanFinder.text as TextSpan;
-
-    expect(textSpan.children![0].style!.fontWeight, FontWeight.bold); // 'My' should be bold.
-    expect(textSpan.children![1].style!.fontWeight, isNot(FontWeight.bold)); // 'Cards' should not be bold.
+    expect(find.text("My Cards", findRichText: true), findsOneWidget);
   });
 }
